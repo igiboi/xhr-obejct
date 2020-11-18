93e414abdee73cf394db68d370da293e44c5b3cc
igiboi <36388765+igiboi@users.noreply.github.com>
2020-11-18 14:53:00 -0500

    Update app.js
    learning methods on how to use xhr and what the response values means

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ function loadData() {
   // OPEN 
   xhr.open('GET', 'data.txt', true);
 
+
+// Optional - Used for spinners/loaders
+xhr.onprogress = function() {
+  console.log('READYSTATE', xhr.readyState);
+}
+
   xhr.onload = function() {
     if(this.status === 200) {
-      console.log(this.responseText)
+      // console.log(this.responseText)
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
     }
   }
+
+// xhr.onreadystatechange = function() {
+//   console.log('READYSTATE', xhr.readyState);
+//   if(this.status === 200 && this.readyState === 4) {
+//     console.log(this.responseText);
+//   }
+// }
+
+// Catch Errors
+xhr.onerror = function() {
+  console.log('Request error...');
+}
 
   xhr.send();
 
